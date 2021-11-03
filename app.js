683a43e616826a48bf5cc86cb1133ab825bdf087
Gamalliel Sharon <you@example.com>
2021-11-03 10:26:55 +0700

    make the video autoplay and adding grain

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const menu = document.querySelector('.btn-menu');
 const menubar = document.querySelector('.menubar');
 const close = document.querySelector('.btn-close');
 
+document.getElementById('vid').play();
+
 menu.addEventListener('click', () => {
   menubar.classList.add('active');
 });
